53f44b2a55bc56c824d8e231c32864363d73d1b6
Sumin <tnalsdldkr2@naver.com>
2025-08-16 22:27:08 +0900

    refactor: 지도 초기 위치를 황리단길 좌표로 변경

--- src/shared/ui/KakaoMap.tsx
+++ src/shared/ui/KakaoMap.tsx
@@ -1,22 +1,11 @@
 import { Map } from 'react-kakao-maps-sdk'
-import { useGeolocation } from '../hooks'
 
-const DEFAULT_CENTER = { lat: 37.5665, lng: 126.978 }
+const HWANGNIDANGIL = { lat: 35.841442, lng: 129.216828 }
 
 const KakaoMap = () => {
-  const { position, loading, error } = useGeolocation()
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (error) {
-    return <div>위치 접근에 실패했습니다: {error}</div>
-  }
-
   return (
     <Map
-      center={position ?? DEFAULT_CENTER}
+      center={HWANGNIDANGIL}
       style={{ width: '100%', height: '100%' }}
       className='flex-3'
       level={7}
