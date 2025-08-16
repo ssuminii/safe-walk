6b37f78a1fb67197ea880d0bd8b4598ad7adfb8a
Sumin <tnalsdldkr2@naver.com>
2025-08-16 21:13:11 +0900

    feat: 로고 변경 및 BrowserRouter 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './globals.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
 )
--- src/shared/ui/Navbar.tsx
+++ src/shared/ui/Navbar.tsx
@@ -1,9 +1,13 @@
 import { SearchBar } from './'
+import { useNavigate } from 'react-router-dom'
+import Logo from '../../assets/logo.svg?react'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+
   return (
     <nav className='flex gap-4 py-[20px] px-6 items-center border-b border-gray-8'>
-      <h1 className='font-family-aggro text-2xl text-primary'>SafeWalk</h1>
+      <Logo className='cursor-pointer' onClick={() => navigate('/')} />
       <SearchBar />
     </nav>
   )
