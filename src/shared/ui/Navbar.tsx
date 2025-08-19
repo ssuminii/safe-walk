381064978577c957f7a48574b31f33661df337a1
Sumin <tnalsdldkr2@naver.com>
2025-08-19 21:26:54 +0900

    feat: 로고 클릭시 새로고침 추가

--- src/shared/ui/Navbar.tsx
+++ src/shared/ui/Navbar.tsx
@@ -1,13 +1,10 @@
 import { SearchBar } from './'
-import { useNavigate } from 'react-router-dom'
 import Logo from '../../assets/logo.svg?react'
 
 const Navbar = () => {
-  const navigate = useNavigate()
-
   return (
     <nav className='flex gap-4 py-[20px] px-6 items-center border-b border-gray-8'>
-      <Logo className='cursor-pointer' onClick={() => navigate('/')} />
+      <Logo className='cursor-pointer' onClick={() => (window.location.href = '/')} />
       <SearchBar />
     </nav>
   )
