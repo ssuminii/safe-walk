ab7e0c8d31632e0cb71ba341a7857cf1cb27e1d7
Sumin <tnalsdldkr2@naver.com>
2025-08-21 15:58:28 +0900

    feat: 관광지 api 추가

---
+++ src/pages/search-page/api/tourist-spot.ts
@@ -0,0 +1,16 @@
+import { apiClient } from '../../../shared/lib/apiClient'
+import type { RegionLabels } from '../../../shared/types/map'
+import type { RegionLabelsParams } from './map'
+
+// 지도 영역 관광지 조회
+export const getTouristSpot = async ({
+  swLat,
+  swLng,
+  neLat,
+  neLng,
+}: RegionLabelsParams): Promise<RegionLabels[]> => {
+  const response = await apiClient.get<RegionLabels[]>('/tourist-spot', {
+    params: { swLat, swLng, neLat, neLng },
+  })
+  return response.data
+}
