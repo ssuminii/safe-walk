a44d1bdd2e1b59dd849c16f5becfd58e2fa311de
Sumin <tnalsdldkr2@naver.com>
2025-08-24 03:47:14 +0900

    feat: 관광지 페이지 UI 구현

--- src/pages/tourist-spot/TouristSpotPage.tsx
+++ src/pages/tourist-spot/TouristSpotPage.tsx
@@ -1,9 +1,36 @@
-import { TouristSideBar } from '@/shared/ui'
+import { KakaoMap, TouristSideBar } from '@/shared/ui'
+import { useCallback, useState } from 'react'
+import { useSearchParams, useLocation } from 'react-router-dom'
+import type { RegionInfoType } from '@/shared/types/map'
 
 export default function TouristSpotPage() {
+  const location = useLocation()
+  const [searchParams] = useSearchParams()
+  const [selectedRegionId, setSelectedRegionId] = useState<string | null>(null)
+  const [selectedAccidentId, setSelectedAccidentId] = useState<string | null>(null)
+  const [accidentInfo, setAccidentInfo] = useState<RegionInfoType | null>(null)
+  const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number } | null>(null)
+  const query = searchParams.get('q')
+  const code = searchParams.get('code')
+
+  const [_accidentList, setAccidentList] = useState<RegionInfoType[]>([])
+
+  const handleAccidentListChange = useCallback((list: RegionInfoType[]) => {
+    setAccidentList(list)
+  }, [])
+
   return (
-    <div>
+    <div className='flex flex-1 h-[calc(100dvh-75px)]'>
       <TouristSideBar />
+      <KakaoMap
+        accidentInfo={accidentInfo}
+        onSelectRegion={setSelectedRegionId}
+        selectedAccidentId={selectedAccidentId}
+        onSelectAccident={setSelectedAccidentId}
+        searchMapCenter={mapCenter}
+        onAccidentListChange={handleAccidentListChange}
+        searchedRegionId={selectedRegionId}
+      />
     </div>
   )
 }
--- src/shared/ui/TouristSideBar.tsx
+++ src/shared/ui/TouristSideBar.tsx
@@ -1,3 +1,77 @@
+import { Dropdown } from '@/shared/ui/Dropdown'
+import Alert from '@/assets/alert.svg?react'
+import { useMemo, useState } from 'react'
+import { TouristSpotTable, type TouristSpot } from '@/shared/ui/TouristSpotTable'
+
+export const provinces = [
+  { value: 'seoul', label: '서울특별시' },
+  { value: 'busan', label: '부산광역시' },
+  { value: 'daegu', label: '대구광역시' },
+  { value: 'incheon', label: '인천광역시' },
+  { value: 'gwangju', label: '광주광역시' },
+  { value: 'daejeon', label: '대전광역시' },
+  { value: 'ulsan', label: '울산광역시' },
+  { value: 'sejong', label: '세종특별자치시' },
+  { value: 'gyeonggi', label: '경기도' },
+  { value: 'gangwon', label: '강원특별자치도' },
+  { value: 'chungbuk', label: '충청북도' },
+  { value: 'chungnam', label: '충청남도' },
+  { value: 'jeonbuk', label: '전북특별자치도' },
+  { value: 'jeonnam', label: '전라남도' },
+  { value: 'gyeongbuk', label: '경상북도' },
+  { value: 'gyeongnam', label: '경상남도' },
+  { value: 'jeju', label: '제주특별자치도' },
+]
+
+export const mockTouristData: TouristSpot[] = [
+  { id: 1, rank: 1, name: '불국사', action: '더보기' },
+  { id: 2, rank: 2, name: '석굴암', action: '더보기' },
+  { id: 3, rank: 3, name: '동궁과 월지', action: '더보기' },
+  { id: 4, rank: 4, name: '첨성대', action: '더보기' },
+  { id: 5, rank: 5, name: '대릉원', action: '더보기' },
+  { id: 6, rank: 6, name: '안압지', action: '더보기' },
+  { id: 7, rank: 7, name: '황리단길', action: '더보기' },
+  { id: 8, rank: 8, name: '국립경주박물관', action: '더보기' },
+]
+
 export default function TouristSideBar() {
-  return <div>TouristSideBar</div>
+  const [selectedProvince, setSelectedProvince] = useState<string | null>(null)
+
+  const handleProvinceChange = (value: string) => {
+    setSelectedProvince(value)
+    console.log('선택된 지역:', value)
+  }
+
+  const filteredData = useMemo(() => {
+    if (!selectedProvince) return []
+    // 추후 api 호출 로직 추가
+    return mockTouristData
+  }, [selectedProvince])
+
+  return (
+    <div className='flex flex-col flex-1 py-4 px-6 gap-[18px]'>
+      <header className='flex flex-col gap-[7px]'>
+        <h2 className='text-[14px] leading-[24px] font-semibold'>인기 관광지 현황</h2>
+        <Dropdown
+          options={provinces}
+          value={selectedProvince}
+          onChange={handleProvinceChange}
+          placeholder='광역시/도'
+        />
+      </header>
+      <div className='flex-1 mb-2'>
+        {!selectedProvince ? (
+          <div className='flex flex-col gap-3 justify-center items-center h-full border border-gray-7'>
+            <Alert />
+            <p className='r2 text-center'>
+              출력할 데이터가 없습니다. <br />
+              위치를 설정하여 확인해보세요.
+            </p>
+          </div>
+        ) : (
+          <TouristSpotTable data={filteredData} />
+        )}
+      </div>
+    </div>
+  )
 }
